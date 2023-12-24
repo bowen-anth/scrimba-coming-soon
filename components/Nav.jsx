3d1937d5c1ade93079c487ba17749058e22dbc40
bowen-anth <bowen.anth@gmail.com>
2023-12-24 13:43:56 -0700

    starter code and nav

---
+++ components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Nav = () => {
+    return (
+        <>
+            <nav className="nav">
+                <img src="public/scrimba-dark-logo.svg" alt="Scrimba logo" width="100px" height="auto"/><span>2.0</span>
+            </nav>
+        </>
+    )
+}
+
+export default Nav
