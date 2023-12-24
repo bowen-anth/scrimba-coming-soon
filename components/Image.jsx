a6d5ca69be2b627d5be849e8690167868225e9c7
bowen-anth <bowen.anth@gmail.com>
2023-12-24 14:12:37 -0700

    added img and css

--- components/Image.jsx
+++ components/Image.jsx
@@ -3,6 +3,12 @@ import React from "react"
 const Image = () => {
     return (
         <>
+            <img className="hero-img"
+            src="../public/peeps-social.svg" 
+            alt="photo of happy cartoon people" 
+            width="400px" 
+            height="auto" 
+            />
         </>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <div className="app-container">
         <Nav />
+        <Image />
       </div>
     </>
   )
