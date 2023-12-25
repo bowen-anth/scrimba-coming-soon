620a90f912913a3979f44c449e6bdd16c122deb6
bowen-anth <bowen.anth@gmail.com>
2023-12-25 14:58:47 -0700

    submit console logs email

--- components/Email.jsx
+++ components/Email.jsx
@@ -1,24 +1,35 @@
 import React from "react"
 
 const Email = () => {
+
+    const [form, setForm] = React.useState({
+        email: ""
+    })
+
     function handleChange(event) {
         const {name, value, type, checked} = event.target
-        setFormData(prevFormData => ({
-            ...prevFormData,
+        setForm(prevForm => ({
+            ...prevForm,
             [name]: type === "checkbox" ? checked : value
         }))
     }
 
         function handleSubmit(event) {
-            console.log(event)
+            console.log(form)
+            event.preventDefault()
         }
 
     return (
         <>
             <form id="form" onSubmit={handleSubmit}>
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" /><br/><br/>
-                <button className="submit-btn">Submit</button>
+                <input type="email" 
+                id="email" 
+                name="email" 
+                onChange={handleChange}
+                value={form.email}
+                /><br/><br/>
+                <button className="submit-btn" onClick={handleSubmit}>Submit</button>
             </form>
         </>
     )
